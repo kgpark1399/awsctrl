b15bc29fb360bf360f2ac5f8b92c293b196d8a20
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-03-15 14:30:40 +0900

    Update monitor.go

--- monitor.go
+++ monitor.go
@@ -2,29 +2,38 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"time"
 )
 
 type C_monitor struct {
-	s_url   string
-	s_rate  string
-	s_ports string
-	s_data  string
+	sUrl    string
+	sPorts  string
+	sData   string
+	sStatus string
+
+	iRate int
 }
 
-func (t *C_monitor) status_check(_s_url string, _s_rate int) {
+func Run_checkUrl(_sUrl string, _iRate int) {
+	cMonitor := New_C_monitor()
+	cMonitor.checkUrl(_sUrl, _iRate)
 
-	resp, err := http.Get(_s_url)
+}
+
+func New_C_monitor() *C_monitor {
+	c := &C_monitor{}
+	return c
+}
+
+func (t *C_monitor) checkUrl(_sUrl string, _iRate int) {
+	resp, err := http.Get(_sUrl)
 	if err != nil {
-		// t.Send("Node01 Server Connection Fail", "Server Connection Fail")
 		log.Fatal(err)
 	}
 
-	ticker := time.NewTicker(time.Second * time.Duration(_s_rate))
+	ticker := time.NewTicker(time.Second * time.Duration(_iRate))
 
 	go func() {
 		for time := range ticker.C {
@@ -32,7 +41,6 @@ func (t *C_monitor) status_check(_s_url string, _s_rate int) {
 			if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
 				fmt.Println("HTTP Status is in the 2xx range", time)
 			} else {
-				// t.Send("Node01 HTTP Status Err", "HTTP Status Err")
 				fmt.Println("Web Error", time)
 			}
 		}
@@ -40,33 +48,5 @@ func (t *C_monitor) status_check(_s_url string, _s_rate int) {
 	time.Sleep(time.Second * 5)
 	ticker.Stop()
 	fmt.Println("Ticker stopped")
-}
-
-func (t *C_monitor) port_check(_s_url, _s_ports string) {
-	address := net.JoinHostPort(_s_url, _s_ports)
-	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
-	if err != nil {
-		fmt.Println(err)
-	} else if conn != nil {
-		defer conn.Close()
-		fmt.Printf("%s:%s is opened \n", _s_url, _s_ports)
-	}
-}
-
-func (t *C_monitor) string_check(_s_url, _s_data string) {
-	resp, err := http.Get(_s_url)
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	// http body 값 변수 저장 및 string 변환
-	data, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(data)
 
 }
