542d61aa0c0605d21cec3b1e3b3ecdc64573393f
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-03-16 19:23:24 +0900

    Update database.go

--- database.go
+++ database.go
@@ -4,29 +4,39 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
-type C_database struct {
-	sDBtype string
-	sDBid   string
-	sDBpwd  string
-	sAddr   string
-	sDBname string
+func Run_GetUrls() {
+	run := C_monitor{}
+	run.GetUrls()
 }
 
-func ConnectDB() {
-	db, err := sql.Open("mysql", "root:devtools1!@tcp(3.34.1.156:3306)/monitor")
-	if err != nil {
-		log.Fatal("database open error:", err)
-	}
-	defer db.Close()
-
-	var name string
-	err = db.QueryRow("SELECT url FROM target WHERE id = 1").Scan(&name)
+func (t *C_monitor) GetUrls() {
+	var website C_monitor
+	db, err := sql.Open("mysql", "root:devtools1!@tcp(ip:port)/monitor")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(name)
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id,url,status FROM target")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var websites []C_monitor
+
+	for rows.Next() {
+		if err := rows.Scan(&website.sId, &website.sUrl, &website.sStatus); err != nil {
+			panic(err.Error())
+		}
+
+		websites = append(websites, website)
+	}
+
+	for _, target := range websites {
+		t.sUrls = append(t.sUrls, target.sUrl)
+	}
+	fmt.Println(t.sUrls)
 }
