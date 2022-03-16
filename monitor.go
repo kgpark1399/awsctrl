f2c220c62eab1fa6223a312cfd33dbcca822923f
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-03-16 19:23:05 +0900

    Update monitor.go

--- monitor.go
+++ monitor.go
@@ -3,57 +3,56 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type C_monitor struct {
+	sId     string
 	sUrl    string
-	sPorts  string
-	sData   string
+	sUrls   []string
 	sStatus string
-
-	iRate int
 }
 
-func Run_checkUrl(_sUrl string, _iRate int) {
-	cMonitor := New_C_checkUrl()
-	cMonitor.checkUrl(_sUrl, _iRate)
-
-}
-
-func New_C_checkUrl() *C_monitor {
-	c := &C_monitor{}
-	return c
-}
-
-func (t *C_monitor) checkUrl(_sUrl string, _iRate int) string {
-
-	// http 상태 조회 , 오류 시 red 변경
+func (t *C_monitor) CheckUrl(_sUrl string, c chan<- C_monitor) {
+	// url get 요청
 	resp, err := http.Get(_sUrl)
-	if err != nil {
-		fmt.Println(err)
-		t.sStatus = "red"
+	status := resp.Status
+	// 에러 발생 또는 상태코드가 400과 같거나 큰 경우 에러처리
+	if err != nil || resp.StatusCode >= 400 {
+		status = "ERR"
+
+	}
+	c <- C_monitor{sUrl: _sUrl, sStatus: status}
+}
+
+func (t *C_monitor) CheckUrl_multi(_sUrl []string) {
+
+	// URL 접속 결과를 담을 map 선언
+	results := make(map[string]string)
+
+	// chanel 생성
+	c := make(chan C_monitor)
+
+	// 인자로 받을 url 접속 시도
+	for _, url := range _sUrl {
+		go t.CheckUrl(url, c)
 	}
 
-	// http 상태 체크 주기 설정
-	ticker := time.NewTicker(time.Second * time.Duration(_iRate))
-
-	// 상태 체크 동작
-	for time := range ticker.C {
-		fmt.Println("HTTP Response Status:", resp.StatusCode, http.StatusText(resp.StatusCode))
-		if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
-			fmt.Println("HTTP Status is in the 2xx range", time)
-			t.sStatus = "green"
-		} else {
-			fmt.Println("Web Error", time)
-			t.sStatus = "red"
-		}
+	// 결과물 map 저장
+	for i := 0; i < len(_sUrl); i++ {
+		result := <-c
+		results[result.sUrl] = result.sStatus
 	}
 
-	// 종료 테스트
-	time.Sleep(time.Second * 5)
-	ticker.Stop()
-	fmt.Println("Ticker stopped")
+	// 결과물 출력
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
 
-	return t.sStatus
+}
+
+func Run_CheckUrl(_sUrl []string) {
+	run := C_monitor{}
+	run.CheckUrl_multi(_sUrl)
 }
