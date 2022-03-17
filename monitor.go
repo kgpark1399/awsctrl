d03e80b0a1d0673cd8897fd269a348fee693848e
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-03-17 18:03:21 +0900

    Update monitor.go

--- monitor.go
+++ monitor.go
@@ -10,49 +10,62 @@ import (
 type C_monitor struct {
 	sId     string
 	sUrl    string
-	sUrls   []string
-	sStatus string
+	sStatus int
+
+	sUrls      []string
+	sStatusGrp []int
 }
 
-func (t *C_monitor) CheckUrl(_sUrl string, c chan<- C_monitor) {
+
+
+// URL HTTP 상태 체크 실행
+func Run_ChkeckUrl() {
+	t := C_monitor{}
+	target := t.GetUrls()
+	for _, url := range target {
+		t.CheckUrl(url)
+	}
+}
+
+// DB URL status 값 체크 실행
+func Run_CheckStatus() {
+	t := C_monitor{}
+	target := t.GetStatus()
+	for _, status := range target {
+		t.CheckStatus(status)
+		fmt.Println(status)
+	}
+}
+
+
+
+// ---------------------------------------------------------------------- //
+
+// URL HTTP 상태 체크 기능
+func (t *C_monitor) CheckUrl(_sUrl string) {
 	// url get 요청
 	resp, err := http.Get(_sUrl)
-	status := resp.Status
+
 	// 에러 발생 또는 상태코드가 400과 같거나 큰 경우 에러처리
 	if err != nil || resp.StatusCode >= 400 {
-		status = "ERR"
+		fmt.Println("STATUS : ERR", "URL :", _sUrl)
+		t.Status(_sUrl)
+
+	} else {
+		fmt.Println("STATUS :", resp.Status, "URL :", _sUrl)
 
 	}
-	c <- C_monitor{sUrl: _sUrl, sStatus: status}
 }
 
-func (t *C_monitor) CheckUrl_multi(_sUrl []string) {
+// DB URL status 값 체크 기능
+func (t *C_monitor) CheckStatus(_sStatus int) {
 
-	// URL 접속 결과를 담을 map 선언
-	results := make(map[string]string)
+	if _sStatus == 0 {
+		fmt.Println("OK")
 
-	// chanel 생성
-	c := make(chan C_monitor)
-
-	// 인자로 받을 url 접속 시도
-	for _, url := range _sUrl {
-		go t.CheckUrl(url, c)
+	} else {
+		fmt.Println("err")
 	}
-
-	// 결과물 map 저장
-	for i := 0; i < len(_sUrl); i++ {
-		result := <-c
-		results[result.sUrl] = result.sStatus
-	}
-
-	// 결과물 출력
-	for url, status := range results {
-		fmt.Println(url, status)
-	}
-
 }
 
-func Run_CheckUrl(_sUrl []string) {
-	run := C_monitor{}
-	run.CheckUrl_multi(_sUrl)
-}
+
