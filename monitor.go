b701fd5a6e1ce87ae3bba89d897c2f3b71d4eccb
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-03-18 18:25:15 +0900

    Update monitor.go

--- monitor.go
+++ monitor.go
@@ -3,41 +3,60 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type C_monitor struct {
-	sId     string
-	sUrl    string
-	sStatus int
+	sId   string
+	sUrl  string
+	SName string
+
+	iStatus int
 
 	sUrls      []string
 	sStatusGrp []int
 }
 
-
-
 // URL HTTP 상태 체크 실행
 func Run_ChkeckUrl() {
-	t := C_monitor{}
-	target := t.GetUrls()
-	for _, url := range target {
-		t.CheckUrl(url)
-	}
+
+	ticker := time.NewTicker(time.Minute * 1)
+	go func() {
+		for t := range ticker.C {
+			_t := C_monitor{}
+			target := _t.GetUrls()
+			for _, url := range target {
+				_t.CheckUrl(url)
+				fmt.Println(t)
+			}
+		}
+	}()
+	time.Sleep(time.Minute * 10)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
 }
 
 // DB URL status 값 체크 실행
 func Run_CheckStatus() {
-	t := C_monitor{}
-	target := t.GetStatus()
-	for _, status := range target {
-		t.CheckStatus(status)
-		fmt.Println(status)
-	}
+
+	ticker := time.NewTicker(time.Minute * 3)
+	go func() {
+		for t := range ticker.C {
+			_t := C_monitor{}
+			target := _t.GetStatus()
+			for _, status := range target {
+				_t.CheckStatus(status)
+				fmt.Println(t)
+			}
+		}
+	}()
+	time.Sleep(time.Minute * 10)
+	ticker.Stop()
+	fmt.Println("Ticker stopped")
+
 }
-
-
 
 // ---------------------------------------------------------------------- //
 
@@ -49,7 +68,7 @@ func (t *C_monitor) CheckUrl(_sUrl string) {
 	// 에러 발생 또는 상태코드가 400과 같거나 큰 경우 에러처리
 	if err != nil || resp.StatusCode >= 400 {
 		fmt.Println("STATUS : ERR", "URL :", _sUrl)
-		t.Status(_sUrl)
+		t.ChagneStatus(_sUrl)
 
 	} else {
 		fmt.Println("STATUS :", resp.Status, "URL :", _sUrl)
@@ -65,7 +84,6 @@ func (t *C_monitor) CheckStatus(_sStatus int) {
 
 	} else {
 		fmt.Println("err")
+
 	}
 }
-
-
