d1a55638e2616ce8b47980906bda0463fb1bb34b
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-03-30 18:40:26 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"sync"
+
+	"monitor/monitor"
+)
+
+func main() {
+	var wait sync.WaitGroup
+	wait.Add(2)
+
+	t := monitor.C_monitor{}
+
+	// DB 접속
+	t.DB_conn("mysql", "root", "devtools1!", "127.0.0.1:3306", "monitor")
+	defer t.DB_close()
+
+	// Monitor 로그 저장
+	// t.Set_logfile("monitor.txt")
+
+	// 메인 기능 동작
+	go t.Monitor__checkUrl(5)
+	go t.Monitor__checkStatus(10)
+
+	wait.Wait()
+}
