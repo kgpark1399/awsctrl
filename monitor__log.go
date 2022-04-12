83af6b1851b615ea0c64d89293c8d01b60eb0763
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-04-12 18:40:32 +0900

    Update monitor__log.go

--- monitor__log.go
+++ monitor__log.go
@@ -1,7 +1,6 @@
 package monitor
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,29 +10,34 @@ type C_monitor__log struct {
 	s_file__name string
 }
 
-func (t *C_monitor__log) Set_monitorlog(_s_file__name string) (os.File, error) {
+// 모니터링 시스템 로그 기록
+func (t *C_monitor__log) Enable_monitor_log(_s_file__name string) error {
 
-	_bool, err := t.Init__log()
+	err := t.Init()
 	if err != nil {
-		fmt.Println(_bool, err)
+		return err
 	}
 
 	file, err := os.OpenFile(_s_file__name, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("[ERROR] Failed to open log file : ", err)
+		return err
 	}
 
+	// 커맨드 출력과 로그 동시 기록
 	multiWriter := io.MultiWriter(file, os.Stdout)
 	log.SetOutput(multiWriter)
 
-	return *file, nil
+	return nil
 }
 
-func (t *C_monitor__log) Init__log() (bool, error) {
+// 모니터링 로그 초기화
+func (t *C_monitor__log) Init() error {
 
 	var err error
 	if err != nil {
-		return false, err
+		log.Println(err)
+		return err
 	}
-	return true, nil
+	return nil
 }
