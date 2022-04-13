c4f530dcd3714b2fa279c4b74c66f7a25f75a3e0
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-04-13 20:28:32 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"log"
 	"monitor/monitor"
+	"os"
+	"os/signal"
 )
+
+func Init() {
+	// 모든 Init 함수들을 호출하여 사전 테스트
+	// 실패 시 프로그램 정지
+}
 
 func main() {
 
 	t := monitor.C_monitor{}
+	t.Conn("mysql", "root", "pwd", "127.0.0.1:3306", "monitor")
+	defer t.Close()
 
-	// config 설정
-	t.Set__db_conn("mysql", "root", "pwd", "127.0.0.1:3306", "monitor")
-	t.Set__log("monitor.txt")
+	t.Enable_log("monitor.txt")
 
-	defer t.DB_close()
+	t.Run_(10)
 
-	t.Run__Monitor(10)
+	channel := make(chan os.Signal, 1)
 
+	signal.Notify(channel)
+
+	for sig := range channel {
+		log.Println("Got signal:", sig)
+		os.Exit(1)
+	}
 }
