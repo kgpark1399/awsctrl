9e41d66eca77c6cb144f2d611469bd175c676f75
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-04-13 20:27:50 +0900

    Update alert__mail.go

--- alert__mail.go
+++ alert__mail.go
@@ -24,36 +24,8 @@ type C_mail struct {
 	arrs_mail__to []string
 }
 
-// 메일 발송 서버 정보 ini 호출 및 SMTP 서버 통신 테스트
-func (t *C_mail) Init() (id, pwd, host, port string, err error) {
-
-	// config ini 파일 읽기
-	cfg, err := ini.Load("config.ini")
-	if err != nil {
-		log.Println("[ERROR] Fail to read config.ini file : ", err)
-		return "", "", "", "", err
-	}
-
-	// 메일 관련 정보 저장
-	t.s_mail__id = cfg.Section("SMTP").Key("S_mail__id").String()
-	t.s_mail__pwd = cfg.Section("SMTP").Key("S_mail__pwd").String()
-	t.s_mail__host = cfg.Section("SMTP").Key("S_mail__host").String()
-	t.s_mail__port = cfg.Section("SMTP").Key("S_mail__port").String()
-
-	smtpserver := t.s_mail__host + ":" + t.s_mail__port
-
-	// SMTP 서버 통신 체크
-	conn, err := net.DialTimeout("tcp", smtpserver, 3*time.Second)
-	if err != nil {
-		log.Println("[ERROR] Fail to connect smtp server : ", err, conn)
-		return "", "", "", "", err
-	}
-
-	return t.s_mail__id, t.s_mail__pwd, t.s_mail__host, t.s_mail__port, nil
-}
-
 // 메일 발송 함수
-func (t *C_mail) Send__mail(_s_mail__body string, arrs_mail__to []string) error {
+func (t *C_mail) Send(_s_mail__body string, arrs_mail__to []string) error {
 
 	// ini 파일 메일 발송 정보 호출
 	id, pwd, host, port, err := t.Init()
@@ -82,13 +54,30 @@ func (t *C_mail) Send__mail(_s_mail__body string, arrs_mail__to []string) error 
 	return err
 }
 
-// 메일 발송 함수 - 구조체x
-func Send__alert_mail(_s_mail__body string, arrs_mail__to []string) error {
+// 메일 발송 서버 정보 ini 호출 및 SMTP 서버 통신 테스트
+func (t *C_mail) Init() (id, pwd, host, port string, err error) {
 
-	t := C_mail{}
-	err := t.Send__mail(_s_mail__body, arrs_mail__to)
-	if err != err {
-		return err
+	// config ini 파일 읽기
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Println("[ERROR] Fail to read config.ini file : ", err)
+		return "", "", "", "", err
 	}
-	return nil
+
+	// 메일 관련 정보 저장
+	t.s_mail__id = cfg.Section("SMTP").Key("S_mail__id").String()
+	t.s_mail__pwd = cfg.Section("SMTP").Key("S_mail__pwd").String()
+	t.s_mail__host = cfg.Section("SMTP").Key("S_mail__host").String()
+	t.s_mail__port = cfg.Section("SMTP").Key("S_mail__port").String()
+
+	smtpserver := t.s_mail__host + ":" + t.s_mail__port
+
+	// SMTP 서버 통신 체크
+	conn, err := net.DialTimeout("tcp", smtpserver, 3*time.Second)
+	if err != nil {
+		log.Println("[ERROR] Fail to connect smtp server : ", err, conn)
+		return "", "", "", "", err
+	}
+
+	return t.s_mail__id, t.s_mail__pwd, t.s_mail__host, t.s_mail__port, nil
 }
