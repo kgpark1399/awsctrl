a659d89f7137330bdd4b2a7fdcb3b54f612a1a9d
kgpark1399 <99658451+kgpark1399@users.noreply.github.com>
2022-04-14 18:20:48 +0900

    Update and rename alert__mail.go to notice__mail.go

--- notice__mail.go
+++ notice__mail.go
@@ -5,27 +5,20 @@ import (
 	"net"
 	"net/smtp"
 	"time"
-
-	"gopkg.in/ini.v1"
 )
 
-type C_mail struct {
+type C_notice__mail struct {
+	c_list_target C_list_target
 
-	// 메일 연동 정보 (ini)
-	s_mail__id   string
-	s_mail__pwd  string
-	s_mail__host string
-	s_mail__port string
-
-	// 메일 발송 옵션
-	s_mail__from string
-	s_mail__body string
+	s_mail__from  string
+	s_mail__title string
+	s_mail__body  string
 
 	arrs_mail__to []string
 }
 
-// 메일 발송 함수
-func (t *C_mail) Send(_s_mail__body string, arrs_mail__to []string) error {
+// 메일 발송
+func (t *C_notice__mail) Send(s_mail__title, _s_mail__body string, arrs_mail__to []string) error {
 
 	// ini 파일 메일 발송 정보 호출
 	id, pwd, host, port, err := t.Init()
@@ -39,7 +32,7 @@ func (t *C_mail) Send(_s_mail__body string, arrs_mail__to []string) error {
 	to := arrs_mail__to
 
 	// 메시지 작성
-	headerSubject := "Subject: [Alert] Server Error\r\n"
+	headerSubject := s_mail__title
 	headerBlank := "\r\n"
 	body := _s_mail__body
 	msg := []byte(headerSubject + headerBlank + body)
@@ -55,22 +48,14 @@ func (t *C_mail) Send(_s_mail__body string, arrs_mail__to []string) error {
 }
 
 // 메일 발송 서버 정보 ini 호출 및 SMTP 서버 통신 테스트
-func (t *C_mail) Init() (id, pwd, host, port string, err error) {
+func (t *C_notice__mail) Init() (id, pwd, host, port string, err error) {
 
-	// config ini 파일 읽기
-	cfg, err := ini.Load("config.ini")
+	id, pwd, host, port, err = t.c_list_target.Get__smtp_acess_info()
 	if err != nil {
-		log.Println("[ERROR] Fail to read config.ini file : ", err)
 		return "", "", "", "", err
 	}
 
-	// 메일 관련 정보 저장
-	t.s_mail__id = cfg.Section("SMTP").Key("S_mail__id").String()
-	t.s_mail__pwd = cfg.Section("SMTP").Key("S_mail__pwd").String()
-	t.s_mail__host = cfg.Section("SMTP").Key("S_mail__host").String()
-	t.s_mail__port = cfg.Section("SMTP").Key("S_mail__port").String()
-
-	smtpserver := t.s_mail__host + ":" + t.s_mail__port
+	smtpserver := host + ":" + port
 
 	// SMTP 서버 통신 체크
 	conn, err := net.DialTimeout("tcp", smtpserver, 3*time.Second)
@@ -79,5 +64,5 @@ func (t *C_mail) Init() (id, pwd, host, port string, err error) {
 		return "", "", "", "", err
 	}
 
-	return t.s_mail__id, t.s_mail__pwd, t.s_mail__host, t.s_mail__port, nil
+	return id, pwd, host, port, nil
 }
